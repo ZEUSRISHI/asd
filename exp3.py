841bfbc4e5314a880e3ae00453c8e10f1ac7fa38
ZEUSRISHI <rishikeshank.22aim@kongu.edu>
2024-04-09 10:00:40 +0530

    gitexp

---
+++ exp3.py
@@ -0,0 +1,15 @@
+def main():
+    # Prompt the user to enter the first number
+    num1 = float(input("Enter the first number: "))
+
+    # Prompt the user to enter the second number
+    num2 = float(input("Enter the second number: "))
+
+    # Calculate the sum
+    result = num1 + num2
+
+    # Display the result
+    print("The sum of", num1, "and", num2, "is:", result)
+
+if __name__ == "__main__":
+    main()
